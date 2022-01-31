291ff357d7c1eb4ee5fb0c598039731a07b10629
fernandoEAD <fernando-danatas@hotmail.com>
2022-01-31 20:45:01 -0300

    fix failures

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,26 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar(){
+
+    return (
+        <header>
+        <nav className="container">
+          <div className="dsmovie-nav-content">
+            <h1>DSMovie</h1>
+            <a href="https://github.com/fernandoEAD">
+                <div className="dsmovie-contact-container">
+
+                </div>
+              <div>
+                <GithubIcon />
+                <p className="dsmovie-contact-link">devsuperior</p>
+              </div>
+            </a>
+          </div>
+        </nav>
+      </header>
+    );
+}
+
+export default Navbar;
