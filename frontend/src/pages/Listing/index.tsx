2eba9cb89744cc62af7015b0dca9973ec2f0f241
fernandoEAD <fernando-danatas@hotmail.com>
2022-02-01 16:28:35 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,10 @@
+import Pagination from "components/Pagination";
+
 function Listing() {
 
-return (
-    <h1>Página de listagem</h1>
-)
+    return (
+        <Pagination />
+    )
 }
 
 export default Listing;
