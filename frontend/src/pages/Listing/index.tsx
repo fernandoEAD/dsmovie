e5d00b33fb09963b31a38e25b0d542431bbd1100
fernandoEAD <fernando-danatas@hotmail.com>
2022-02-09 21:10:37 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -29,17 +29,13 @@ function Listing() {
             });
     }, [pageNumber]);
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
-            <Pagination />
+            <Pagination page ={page} onChange={handlePageChange}/>
 
             <div className="container">
                 <div className="row">
